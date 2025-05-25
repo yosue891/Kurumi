502fa4f2884d72f0923ffd04c303107b3a5804d3
Ado <adonaylester85@gmail.com>
2025-05-25 17:39:20 -0600

    Actualizar buscador-animesearch.js

--- plugins/buscador-animesearch.js
+++ plugins/buscador-animesearch.js
@@ -36,5 +36,5 @@ handler.tags = ['buscador'];
 handler.command = ['animeflvsearch', 'animeflv', 'animesearch'];
 handler.register = true;
 handler.premium = true
-handler.estrellas = 7;
+handler.estrellas = 0;
 export default handler;
