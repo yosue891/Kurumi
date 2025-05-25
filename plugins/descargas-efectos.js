dca78aa21b8fe9267e3e393047937af11775f84b
Ado <adonaylester85@gmail.com>
2025-05-25 17:43:58 -0600

    Actualizar descargas-efectos.js

--- plugins/descargas-efectos.js
+++ plugins/descargas-efectos.js
@@ -54,7 +54,7 @@ handler.help = ['bass', 'blown', 'deep', 'earrape', 'fast', 'fat', 'nightcore', 
 handler.tags = ['descargas']
 handler.command = /^(bass|blown|deep|earrape|fas?t|nightcore|reverse|robot|slow|smooth|tupai|squirrel|chipmunk|reverb|chorus|flanger|distortion|pitch|highpass|lowpass|underwater)$/i
 handler.register = true 
-handler.estrellas = 6;
+handler.estrellas = 0;
 export default handler
 
 const getRandom = (ext) => {
