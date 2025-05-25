93d290a78910194ee38d78660a85a98caa504332
Ado <adonaylester85@gmail.com>
2025-05-25 17:45:27 -0600

    Actualizar descargas-npmdl.js

--- plugins/descargas-npmdl.js
+++ plugins/descargas-npmdl.js
@@ -59,6 +59,6 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 handler.help = ["npmdl"];
 handler.tags = ["descargas"];
 handler.command = ["npmdownloader", "npmdownload", "npmpkgdownloader", "npmpkgdownload", "npmdl", "npmd"];
-handler.estrellas = 14;
+handler.estrellas = 2;
 
 export default handler;
