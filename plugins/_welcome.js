4e3fd0937b271e6b4026d99f407410a262331b75
Ado <adonaylester85@gmail.com>
2025-05-25 17:26:00 -0600

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -7,22 +7,25 @@ export async function before(m, { conn, participants, groupMetadata }) {
   let who = m.messageStubParameters[0]
   let taguser = `@${who.split('@')[0]}`
   let chat = global.db.data.chats[m.chat]
-  let defaultImage = 'https://files.catbox.moe/nnz4de.jpg;
+  let defaultImage = 'https://files.catbox.moe/nnz4de.jpg' // ← Aquí faltaba cerrar comillas
 
   if (chat.welcome) {
-    let img;
+    let img
     try {
-      let pp = await conn.profilePictureUrl(who, 'image');
-      img = await (await fetch(pp)).buffer();
+      let pp = await conn.profilePictureUrl(who, 'image')
+      img = await (await fetch(pp)).buffer()
     } catch {
-      img = await (await fetch(defaultImage)).buffer();
+      img = await (await fetch(defaultImage)).buffer()
     }
 
     if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_ADD) {
-    let bienvenida = `┌─❦ 𝐙𝐞𝐫𝐨 𝐓𝐰𝐨\n│「 Welcome 」\n└┬❦ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │😼 ${welmss}\n   │😼  ${groupMetadata.subject}\n   └───────────────┈ ⳹\n> ${dev}`
+      let bienvenida = `┌─❦ 𝐙𝐞𝐫𝐨 𝐓𝐰𝐨\n│「 Welcome 」\n└┬❦ 「 ${taguser} 」\n   │😼 ${welmss}\n   │😼 ${groupMetadata.subject}\n   └───────────────┈ ⳹\n> ${dev}`
       await conn.sendMessage(m.chat, { image: img, caption: bienvenida, mentions: [who] }, { quoted: estilo })
-    } else if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_REMOVE || m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_LEAVE) {
-     let bye = `┌─❦ 𝐙𝐞𝐫𝐨 𝐓𝐰𝐨\n│「 ADIOS 👋 」\n└┬❦ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │😒  Se fue\n   │😒 Jamás te quisimos aquí inutil\n   └───────────────┈ ⳹\n> ${dev}`
+    } else if (
+      m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_REMOVE ||
+      m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_LEAVE
+    ) {
+      let bye = `┌─❦ 𝐙𝐞𝐫𝐨 𝐓𝐰𝐨\n│「 ADIOS 👋 」\n└┬❦ 「 ${taguser} 」\n   │😒 Se fue\n   │😒 Jamás te quisimos aquí inútil\n   └───────────────┈ ⳹\n> ${dev}`
       await conn.sendMessage(m.chat, { image: img, caption: bye, mentions: [who] }, { quoted: estilo })
     }
   }
