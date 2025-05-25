7d9ed27a31b06fa20e6189d47c9cd600336dd191
Ado <adonaylester85@gmail.com>
2025-05-25 17:41:23 -0600

    Actualizar buscador-wikipedia.js

--- plugins/buscador-wikipedia.js
+++ plugins/buscador-wikipedia.js
@@ -20,7 +20,7 @@ let handler = async (m, { text }) => {
 handler.help = ['wikipedia']
 handler.tags = ['buscador']
 handler.command = ['wiki','wikipedia'] 
-handler.estrellas = 7;
+handler.estrellas = 2;
 
 
 export default handler
