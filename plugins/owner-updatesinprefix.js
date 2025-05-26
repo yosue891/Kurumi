b99dd0334814a342224fb19e3a470de6eb401d0f
Ado <adonaylester85@gmail.com>
2025-05-25 18:49:28 -0600

    Crear owner-updatesinprefix.js

---
+++ plugins/owner-updatesinprefix.js
@@ -0,0 +1,14 @@
+import { execSync } from 'child_process'
+let handler = async (m, { conn, text }) => {
+await m.react('🕓')
+if (conn.user.jid == conn.user.jid) {
+let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
+await conn.reply(m.chat, stdout.toString(), m)
+await m.react('✅')
+}}
+handler.help = ['update']
+handler.tags = ['owner']
+handler.customPrefix = /^(update|Update)$/i
+handler.command = new RegExp
+
+export default handler
