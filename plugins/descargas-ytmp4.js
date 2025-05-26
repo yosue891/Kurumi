de5b12f8ddf247c05762a4608bb7fa1c35493414
Ado <adonaylester85@gmail.com>
2025-05-25 18:58:51 -0600

    Actualizar descargas-ytmp4.js

--- plugins/descargas-ytmp4.js
+++ plugins/descargas-ytmp4.js
@@ -10,20 +10,22 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     let api = await (await fetch(`https://api.siputzx.my.id/api/d/ytmp4?url=${text}`)).json();
     let dl_url = api.data.dl;
 
-    await conn.sendMessage(m.chat, { video: { url: dl_url }, caption: `${resp}` },{ quoted: m });
+    await conn.sendMessage(m.chat, {
+      video: { url: dl_url },
+      caption: `✅ Video descargado correctamente./n> ❀ Send By Zero Two`,
+    }, { quoted: m });
 
     await m.react('✅');
   } catch (error) {
     console.error(error);
     await m.react('❌');
-    conn.reply(m.chat, `✖️ error comando mal usado ${usedPrefix + commando} *<url>*  `, m, rcanal);
+    conn.reply(m.chat, `✖️ Error al usar el comando ${usedPrefix + command} *<url>*`, m, rcanal);
   }
 };
 
 handler.help = ['ytmp4 *<url>*'];
 handler.tags = ['descargas'];
 handler.command = ['ytmp4', 'ytv'];
-handler.estrellas = 0;
 handler.register = true;
 
 export default handler;
